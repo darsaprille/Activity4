02250a2ace8b6f8fbb45e14d63db8a9d5add3a38
ayessaenderez <enderezayessa06@gmail.com>
2023-11-09 16:04:07 +0800

    feat create table animals

--- Veterinary.sql
+++ Veterinary.sql
@@ -1,11 +1,15 @@
 CREATE DATABASE veterinary;
 
-CREATE TABLE IF NOT EXISTS public.owners
+CREATE TABLE IF NOT EXISTS public.animals
 (
-owner_id INT PRIMARY KEY,
-ofirstname VARCHAR (50) NOT NULL,
-olastname VARCHAR (50) NOT NULL,
-address VARCHAR (50) NOT NULL,
-phone VARCHAR (15) NOT NULL,
-email VARCHAR (100) NOT NULL
+animal_id INT NOT NULL,
+name VARCHAR(50) NOT NULL,
+species VARCHAR(50) NOT NULL,
+breed VARCHAR(50) NOT NULL,
+date_of_birth DATE NOT NULL,
+gender VARCHAR(10) NOT NULL,
+color VARCHAR(50) NOTY NULL,
+owner_id INT NOT NULL,
+PRIMARY KEY(animalid),
+FOREIGN KEY (owner_id) REFERENCES owners(owner_id)
 );
