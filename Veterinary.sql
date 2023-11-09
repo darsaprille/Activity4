908d45d302ebd9de3289b0535edbd5f2fe21dc09
darsaprille <adarleneaprille423@gmail.com>
2023-11-09 16:50:26 +0800

    feat insert data appointments

--- Veterinary.sql
+++ Veterinary.sql
@@ -86,3 +86,16 @@ VALUES(1,'Fido','Dog','Golden Retriever',2018-03-15,'Male','Golden',1),
 (8,'Simba','Cat','Bengal',2010-06-10,'Male','Spotted Rosetted',8),
 (9,'Max','Dog','Dachshund',2020-07-14,'Male','Black and Tan',9),
 (10,'Cleo','Cat','Ragdoll',2019-0512-22,'Female','Seal Point',10);
+
+INSERT INTO owners (ownerid, ofirstname, olastname, address, phone, email)
+VALUES
+    (1, 'John', 'Doe', '123 Main St', '555-555-1212', 'johndoe@example.com'),
+    (2, 'Jane', 'Doe', '456 Elm St', '555-555-1212', 'janedoe@example.com'),
+    (3, 'Bob', 'Smith', '789 Oak St', '555-555-1212', 'bobsmith@example.com'),
+    (4, 'Sue', 'Jones', '321 Pine St', '555-555-1212', 'suejones@example.com'),
+    (5, 'Tom', 'Brown', '654 Maple St', '555-555-1212', 'tombrown@example.com'),
+    (6, 'Mary', 'Green', '987 Cedar St', '555-555-1212', 'marygreen@example.com'),
+    (7, 'Bill', 'White', '246 Birch St', '555-555-1212', 'billwhite@example.com'),
+    (8, 'Amy', 'Black', '135 Walnut St', '555-555-1212', 'amyblack@example.com'),
+    (9, 'Mike', 'Gray', '864 Cherry St', '555-555-1212', 'mikegray@example.com'),
+    (10, 'Lisa', 'Taylor', '975 Spruce St', '555-555-1212', 'lisataylor@example.com');
